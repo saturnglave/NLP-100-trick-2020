f6765b8feddc4f8216e239fd103283b5b20753ee
saturn <45328849+saturnglave@users.noreply.github.com>
2020-04-27 21:43:39 +0900

    Chap1 おわり

--- Chapter1/CH1-09.py
+++ Chapter1/CH1-09.py
@@ -1,1 +1,15 @@
 # coding:utf-8
+import random
+
+s = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+ans = ''
+
+for item in s.split():
+    if len(item) <= 4:
+        ans += item
+    else:
+        tmp = ''.join(random.sample(item[1:-1], len(item) - 2))
+        ans += item[0] + tmp + item[-1]
+    ans += ' '
+
+print(ans)
